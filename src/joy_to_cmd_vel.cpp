e808f8cb354445f047e5823305dc8d2ad0076bf8
mjlee111 <menggu1234@naver.com>
2023-07-25 22:23:54 +0900

    update

--- src/joy_to_cmd_vel.cpp
+++ src/joy_to_cmd_vel.cpp
@@ -6,14 +6,14 @@ JoyToCmdVelConverter::JoyToCmdVelConverter()
 
     joy_sub_ = nh_.subscribe("/joy", 10, &JoyToCmdVelConverter::joyCallback, this);
 
-    cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/joy_to_cmd", 10);
 }
 
 void JoyToCmdVelConverter::joyCallback(const sensor_msgs::Joy::ConstPtr &joy_msg)
 {
     geometry_msgs::Twist cmd_vel_msg;
     cmd_vel_msg.linear.x = joy_msg->axes[1];
-    cmd_vel_msg.angular.z = joy_msg->axes[0];
+    cmd_vel_msg.angular.z = joy_msg->axes[3];
 
     cmd_vel_pub_.publish(cmd_vel_msg);
 }
